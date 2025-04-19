f1857e8cf5fb78ef475621afe46b6ff2c60ffd37
rafael-gama <rafaelantunesjf@gmail.com>
2025-04-18 22:20:59 -0300

    feat: implementacao da sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { Header } from './components/Header'
 import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
 import  './global.css'
+
 
 function App() {
   return (
     <>
       <Header />
-      <Post 
-        author='Rafael Antunes'
-        content='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis nihil itaque earum, libero odit commodi accusamus id labore, amet facilis ab iusto ut, pariatur deserunt molestias aliquid quaerat sed exercitationem!'
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author='Rafael Antunes'
+            content='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis nihil itaque earum, libero odit commodi accusamus id labore, amet facilis ab iusto ut, pariatur deserunt molestias aliquid quaerat sed exercitationem!'
+          />
+        </main>
+      </div>
+
+      
     </>
   )
 }
