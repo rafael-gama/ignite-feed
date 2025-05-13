61c5ba6b090a081ea2e63a437433b9aa851684a2
rafael-gama <rafaelantunesjf@gmail.com>
2025-05-12 21:57:13 -0300

    feat: implementacao da validacao dos comentarios e operacao de delete

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,36 @@ import { Sidebar } from './components/Sidebar'
 import styles from './App.module.css'
 import  './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/rafael-gama.png',
+      name: 'Rafael Antunes',
+      role: 'Software Engineer'
+    }, 
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content:'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2025-04-26 20:16:00'), 
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @ Rocketseat'
+    }, 
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content:'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2025-04-10 20:00:00'), 
+  },
+]
 
 function App() {
   return (
@@ -14,14 +44,16 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author='Rafael Antunes'
-            content='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis nihil itaque earum, libero odit commodi accusamus id labore, amet facilis ab iusto ut, pariatur deserunt molestias aliquid quaerat sed exercitationem!'
-          />
-           <Post 
-            author='Rafael Antunes'
-            content='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis nihil itaque earum, libero odit commodi accusamus id labore, amet facilis ab iusto ut, pariatur deserunt molestias aliquid quaerat sed exercitationem!'
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
 
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,22 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react';
 
-export function Comment(){
+
+export function Comment({content, onDeleteComment}){
+    const [likeCount, setLikeCount] = useState(0);
+    
+    function handleDeleteComment(){
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment(){
+        setLikeCount((state) => {
+            return state + 1;
+        });
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar 
@@ -23,19 +37,22 @@ export function Comment(){
                             </time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button 
+                            onClick={handleDeleteComment}  
+                            title="Deletar comentário"
+                        >
                             <Trash size={24}/>
                         </button>
                     </header>
                     <p>
-                        Muito bom Devon, parabéns!! 👏👏
+                       {content}
                     </p>
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
 
